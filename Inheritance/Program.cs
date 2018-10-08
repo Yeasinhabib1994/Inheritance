4168013f97bd51e8ea7fa562fe661eb98f995f47
yeasin habib <yeasinhabib1994@gmail.com>
2018-10-08 17:08:35 +0600

    Add project files.

---
+++ Inheritance/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = new Text();
+            text.Width = 100;
+            text.Height = 50;
+            text.FontSize = 50;
+            text.AddHyperlink("https://github.com/");
+            text.Copy();
+            text.Duplicate();
+        }
+    }
+
+    public class PresentationObject
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public void Copy()
+        {
+            Console.WriteLine("Object copied to clipboard.");
+        }
+
+        public void Duplicate()
+        {
+            Console.WriteLine("Object was duplicated,");
+        }
+    }
+
+    public class Text : PresentationObject
+    {
+        public int FontSize { get; set; }
+        public string FontName { get; set; }
+
+        public void AddHyperlink(string url)
+        {
+            Console.WriteLine("We added a link to " + url);
+        }
+    }
+}
